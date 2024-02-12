39b82717932d6085d964544a2093219d99e918f7
Patricio Avila <pavila@vialterna.com>
2024-02-12 17:56:46 -0600

    design home page

---
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -0,0 +1,34 @@
+import { Box, Flex, Text, VStack } from "@chakra-ui/react"
+import SuggestedHeader from "./SuggestedHeader"
+import SuggestedUser from "./SuggestedUser"
+import { Link } from "react-router-dom"
+
+const SuggestedUsers = () => {
+  return (
+    <VStack py={8} px={6} gap={4}>
+        <SuggestedHeader />
+
+        <Flex alignItems={'center'} justifyContent={'space-between'} w={'full'}>
+            <Text fontSize={12} fontWeight={'bold'} color={'gray.500'}>
+                Suggested for you
+            </Text>
+            <Text fontSize={12} fontWeight={'bold'} _hover={{color:'gray.400'}} cursor={'pointer'}>
+                See all
+            </Text>
+        </Flex>
+
+        <SuggestedUser name='Dan Abrahmov' followers={1392} avatar='https://bit.ly/dan-abramov'/>
+        <SuggestedUser name='Ryan Florence' followers={567} avatar='https://bit.ly/ryan-florence'/>
+        <SuggestedUser name='Christian Nwamba' followers={759} avatar='https://bit.ly/code-beast'/>
+
+        <Box fontSize={12} color={'gray.500'} mt={5} alignSelf={'start'}>
+            2023 built by {' '}
+            <Link href={''} target='_blank' color= "blue.500" fontSize={14}>
+                Patoneitor
+            </Link>
+        </Box>
+    </VStack>
+  )
+}
+
+export default SuggestedUsers
