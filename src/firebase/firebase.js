c431a845ab47a3b9fd474ba695c7d15b6ccd45dc
Patricio Avila <pavila@vialterna.com>
2024-02-15 15:52:31 -0600

    firebase setup

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage, getstorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBq9FC2TwdCO49fN340OwCrqcgSEo_Opac",
+  authDomain: "insta-clone-devf.firebaseapp.com",
+  projectId: "insta-clone-devf",
+  storageBucket: "insta-clone-devf.appspot.com",
+  messagingSenderId: "616312283886",
+  appId: "1:616312283886:web:df50157c744567faf949ff",
+  measurementId: "G-TMJ81TM02F"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const firestore = getFirestore(app)
+const storage = getStorage(app)
+
+export { app, auth, firestore, storage }
