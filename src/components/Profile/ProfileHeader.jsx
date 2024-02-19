404fbb72edbec37a5e786ae2a065f63a5a53217c
Patricio Avila <pavila@vialterna.com>
2024-02-19 12:50:00 -0600

    fetching profile header data

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,6 +1,13 @@
 import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from "@chakra-ui/react"
+import useUserProfileStore from "../../store/userProfileStore"
+import useAuthStore from "../../store/authStore"
 
 const ProfileHeader = () => {
+    const {userProfile} = useUserProfileStore()
+    const authUser = useAuthStore(state => state.user)
+    const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username
+    const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username
+
   return (
     <Flex gap={{base:4, sm:10}} py={10} direction={{base:'column', sm:'row'}}>
         <AvatarGroup
@@ -9,44 +16,56 @@ const ProfileHeader = () => {
             alignSelf={'flex-start'}
             mx={'auto'}
         >
-            <Avatar name="Patoneitor" src='/profilepic.png' alt='Patoneitor logo'/>
+            <Avatar src={userProfile.profilePicURL} alt='Patoneitor logo'/>
         </AvatarGroup>
 
         <VStack alignItems={'start'} gap={2} mx={'auto'} flex={1}>
             <Flex 
-                gap={4} direction={{base:'column', sm:'row'}}
+                gap={4}
+                direction={{base:'column', sm:'row'}}
                 justifyContent={{base:'center', sm:'flex-start'}}
                 alignItems={'center'}
                 w={'full'}
             >
                 <Text fontSize={{base:'sm', sm:'lg'}}>
-                    Patoneitor_
+                    {userProfile.username}
                 </Text>
-                <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
-                    <Button bg={'white'} color={'black'} _hover={{bg:'whiteAlpha.800'}} size={{base:'xs', md:'sm'}}>
-                        Edit Profile
-                    </Button>
-                </Flex>
+
+                {visitingOwnProfileAndAuth && (
+                    <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
+                        <Button bg={'white'} color={'black'} _hover={{bg:'whiteAlpha.800'}} size={{base:'xs', md:'sm'}}>
+                            Edit Profile
+                        </Button>
+                    </Flex>
+                )}
+
+                {visitingAnotherProfileAndAuth && (
+                    <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
+                        <Button bg={'blue.500'} color={'white'} _hover={{bg:'blue.600'}} size={{base:'xs', md:'sm'}}>
+                            Follow
+                        </Button>
+                    </Flex>
+                )}
             </Flex>
 
             <Flex alignItems={'center'} gap={{base:2, sm:4}}>
                 <Text fontSize={{base:'xs', md:'sm'}}>
-                    <Text as='span' fontWeight={'bold'} mr={1}>4</Text>
+                    <Text as='span' fontWeight={'bold'} mr={1}>{userProfile.posts.length}</Text>
                     Posts
                 </Text>
                 <Text fontSize={{base:'xs', md:'sm'}}>
-                    <Text as='span' fontWeight={'bold'} mr={1}>149</Text>
+                    <Text as='span' fontWeight={'bold'} mr={1}>{userProfile.followers.length}</Text>
                     Followers
                 </Text>
                 <Text fontSize={{base:'xs', md:'sm'}}>
-                    <Text as='span' fontWeight={'bold'} mr={1}>175</Text>
+                    <Text as='span' fontWeight={'bold'} mr={1}>{userProfile.following.length}</Text>
                     Following
                 </Text>
-                <Flex alignItems={'center'} gap={4}>
-                    <Text fontSize={'sm'} fontWeight={'bold'}>Patoneitor</Text>
-                </Flex>
             </Flex>
-            <Text fontSize={'sm'}>Tutorials that are meant to level up your skills as a programmer.</Text>
+            <Flex alignItems={'center'} gap={4}>
+                <Text fontSize={'sm'} fontWeight={'bold'}>{userProfile.fullName}</Text>
+            </Flex>
+            <Text fontSize={'sm'}>{userProfile.bio}</Text>
         </VStack>
     </Flex>
   )
